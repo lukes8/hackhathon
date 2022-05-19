723526c9e09ec8302c9c90218f9c4daeb734c882
Petr Semenisin <petr.semenisin@1und1.de>
2022-05-19 15:46:56 +0200

    Another basic files (dtos, donation service, donation controller)

--- server/src/main/java/com/hackhathon/darujemejidlo/controller/DonationController.java
+++ server/src/main/java/com/hackhathon/darujemejidlo/controller/DonationController.java
@@ -1,5 +1,36 @@
 package com.hackhathon.darujemejidlo.controller;
 
+
+import org.jetbrains.annotations.NotNull;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hackhathon.darujemejidlo.dto.DonationWriteRequestDto;
+import com.hackhathon.darujemejidlo.dto.DonationWriteResponseDto;
+import com.hackhathon.darujemejidlo.persistence.entity.Donation;
+import com.hackhathon.darujemejidlo.service.DonationService;
+
+@RestController
+@RequestMapping(path = "/donation",
+        produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class DonationController {
 
+    private final DonationService donationService;
+
+    public DonationController(DonationService donationService) {
+        this.donationService = donationService;
+    }
+
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DonationWriteResponseDto> postDonation (@NotNull @RequestBody DonationWriteRequestDto donationWriteRequestDto) {
+
+        return new ResponseEntity<DonationWriteResponseDto>(donationService.createDonation(donationWriteRequestDto), HttpStatus.ACCEPTED);
+    }
 }
--- server/src/main/java/com/hackhathon/darujemejidlo/service/DonationService.java
+++ server/src/main/java/com/hackhathon/darujemejidlo/service/DonationService.java
@@ -1,5 +1,16 @@
 package com.hackhathon.darujemejidlo.service;
 
+import org.springframework.stereotype.Component;
+
+import com.hackhathon.darujemejidlo.dto.DonationWriteRequestDto;
+import com.hackhathon.darujemejidlo.dto.DonationWriteResponseDto;
+
+@Component
 public class DonationService {
+
+    public DonationWriteResponseDto createDonation(DonationWriteRequestDto donationWriteRequestDto) {
+
+
+    }
 
 }
