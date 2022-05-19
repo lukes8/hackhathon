3f91307d35d176d8d9714724c6611400c1bd06ef
Petr Semenisin <petr.semenisin@1und1.de>
2022-05-19 15:24:15 +0200

    Controller & Services Mocks

---
+++ server/src/main/java/com/hackhathon/darujemejidlo/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.hackhathon.darujemejidlo.controller;
+
+public class LoginController {
+
+}
---
+++ server/src/main/java/com/hackhathon/darujemejidlo/service/UserService.java
@@ -0,0 +1,5 @@
+package com.hackhathon.darujemejidlo.service;
+
+public class UserService {
+
+}
